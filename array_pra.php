77781fe63ae6be25e2ae9f5b6ef2a6ba86ec53ca
PYU <h261922@gmail.com>
2020-10-29 16:14:47 +0800

    作業

---
+++ array_pra.php
@@ -0,0 +1,57 @@
+<h1>五百年內的閏年</h1>
+<?php
+$year=date("Y");
+$array=[];
+
+for($i=0;$i<500;$i++){
+
+    if(($year+$i)%4 == 0 && (($year+$i)%100 != 0  || (($year+$i)%400==0 && ($year+$i) % 3200 !=0))){
+
+        $array[]=$year+$i;
+        
+    }   
+}
+
+echo "五百年內的閏年:<br>";
+foreach($array as $year){
+    echo $year ."<br>";
+}
+
+echo "一共有".count($array)."個閏年";
+
+?>
+<h1>天干地支</h1>
+<?php
+
+$sky=['甲','乙','丙','丁','戊','己','庚','辛','壬','癸'];
+$land=['子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥'];
+$match=[];
+foreach($sky as $i=> $s){   //$i key $s value
+
+  foreach($land as $j=> $l){
+    $match[1024+($i*10+$j)]=$s.$l;   //後重複值覆蓋，錯誤
+  }
+}
+
+print_r($match);
+echo $match[1096];
+
+?>
+
+<h1>陣列元素反轉</h1>
+<?php
+$a=[2,4,6,1,8];
+echo "原陣列:";
+echo "<pre>";
+print_r($a);
+echo"</pre>";
+
+for($i=0;$i<ceil(count($a)/2);$i++){
+  $tmp;
+  $tmp=$a[$i];
+  $a[$i]=$a[count($a)-1-$i];
+  $a[count($a)-1-$i]=$tmp;
+  
+}
+
+?>
